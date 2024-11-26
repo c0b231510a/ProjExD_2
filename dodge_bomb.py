8b49c096e5faf815331047743eb8b95be7519198
ryuu <c0b231510a@edu.teu.ac.jp>
2024-11-26 14:49:39 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,7 +3,6 @@ import random
 import sys
 import pygame as pg
 
-
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
     pg.K_UP:    (0, -5),
@@ -13,6 +12,16 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -21,8 +30,9 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    bb_img = pg.Surface((20,20))
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0))
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
@@ -30,34 +40,37 @@ def main():
     vx, vy = 5, 5
 
     clock = pg.time.Clock()
-    tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
 
+        screen.blit(bg_img, [0, 0])
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, delta in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += delta[0]
                 sum_mv[1] += delta[1]
-            
         kk_rct.move_ip(sum_mv)
 
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
         bb_rct.move_ip(vx, vy)
-        if bb_rct.left < 0 or bb_rct.right > WIDTH:
+
+        x_bound, y_bound = check_bound(bb_rct)
+        if not x_bound:
             vx = -vx
-        if bb_rct.top < 0 or bb_rct.bottom > HEIGHT:
+        if not y_bound:
             vy = -vy
 
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
-        pg.display.update()
-        tmr += 1
-        clock.tick(50)
 
+        pg.display.update()
+        clock.tick(50)
 
 if __name__ == "__main__":
     pg.init()
